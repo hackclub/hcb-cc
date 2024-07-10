49404d2f8b0058e46e610fa56df8794c81aced06
Sam Poder <sam.r.poder@gmail.com>
2024-07-10 08:12:09 -0700

    Update event.jsx

--- components/images/event.jsx
+++ components/images/event.jsx
@@ -36,7 +36,7 @@ function Image({ logo, name, preview = false }) {
             gap: '16px',
             justifyContent: 'flex-end'
         }}>
-            <div style={{ fontSize: calculateFontSize(name), textAlign: 'right', wordBreak: 'break-all', textWrap: 'balance'}}>
+            <div style={{ fontSize: calculateFontSize(name), textAlign: 'right', textWrap: 'balance', flexGrow: 1}}>
               {name}
             </div>
             <img src={logo} style={{marginRight: '0px', height: '200px', width: '200px', height: '200px'}} />
